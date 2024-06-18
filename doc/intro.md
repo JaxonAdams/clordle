f2085adfb2dc2d3144c894beb26d702603a44a64
Jaxon Adams <jadamsresume1@gmail.com>
2024-06-17 20:29:06 -0600

    initial files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clordle
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clordle/core.clj
@@ -0,0 +1,8 @@
+(ns clordle.core
+  (:gen-class))
+
+;; ...Engage!
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& _]
+  (println "Hello, World!"))
