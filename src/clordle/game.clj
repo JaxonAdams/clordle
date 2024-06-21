87e455cde2054687d452a16138c4d8b2047fef47
Jaxon Adams <jadamsresume1@gmail.com>
2024-06-21 14:22:53 -0600

    refactored some logic around the daily word

--- src/clordle/game.clj
+++ src/clordle/game.clj
@@ -2,19 +2,21 @@
   (:require [clojure.string :as clj-str]
             [clojure.java.io :as io]))
 
-(def words (-> "words.txt" io/resource slurp clj-str/split-lines))
+(defn get-words 
+  []
+  (-> "words.txt" io/resource slurp clj-str/split-lines))
 
 (defn daily-word
   "Return today's 'word of the day'."
   []
-  (-> (java.time.Instant/now)  ;; I want to map the current date to a word in clordle.game/words
+  (-> (java.time.Instant/now)    ;; I want to map the current date to a word in our list of words
       inst-ms
       (quot 1000)
       (quot 60)
       (quot 60)
-      (quot 24)                ;; number of days since Jan 1, 1970
-      (mod (count words))      ;; use modulo to map to an element in clordle.game/words
-      (->> (nth words))))
+      (quot 24)                  ;; number of days since Jan 1, 1970
+      (mod (count (get-words)))  ;; use modulo to map to an element in our list of words
+      (->> (nth (get-words)))))
 
 (defn check-character
   [chosen-word character idx]
@@ -28,20 +30,20 @@
   (map-indexed (fn [i c] [i c]) word))
 
 (defn check-solution
-  [chosen-word guess]
+  [guess]
   (->> guess
        index-characters
-       (map (fn [[idx character]] (check-character chosen-word character idx)))))
+       (map (fn [[idx character]] (check-character (daily-word) character idx)))))
 
 (comment
-  (check-solution "hello" "stair")
-  (check-solution "hello" "stare")
-  (check-solution "hello" "heyyo")
-  (check-solution "hello" "hello"))
+  (check-solution "stair")
+  (check-solution "stare")
+  (check-solution "heyyo")
+  (check-solution "hello"))
 
 (comment
-  (check-solution (daily-word) "stare"))
+  (check-solution "stare"))
 
 (comment
-  (vec words)
+  (vec (get-words))
   (daily-word))
