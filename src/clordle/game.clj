e47fb4096ffcb06f833f82bb40b99c9e27ad021d
Jaxon Adams <jadamsresume1@gmail.com>
2024-06-21 14:12:43 -0600

    added logic for selecting daily word

--- src/clordle/game.clj
+++ src/clordle/game.clj
@@ -1,17 +1,27 @@
 (ns clordle.game
-  (:require clojure.string))
+  (:require [clojure.string :as clj-str]
+            [clojure.java.io :as io]))
 
-;; random function will be replaced later
+(def words (-> "words.txt" io/resource slurp clj-str/split-lines))
+
 (defn daily-word
+  "Return today's 'word of the day'."
   []
-  (rand-nth ["hello" "stare" "place" "cares" "happy"]))
+  (-> (java.time.Instant/now)  ;; I want to map the current date to a word in clordle.game/words
+      inst-ms
+      (quot 1000)
+      (quot 60)
+      (quot 60)
+      (quot 24)                ;; number of days since Jan 1, 1970
+      (mod (count words))      ;; use modulo to map to an element in clordle.game/words
+      (->> (nth words))))
 
 (defn check-character
   [chosen-word character idx]
   (cond
-    (= character (nth chosen-word idx)) :green
-    (contains? (set chosen-word) character) :yellow
-    :else :red))
+    (= character (nth chosen-word idx)) {(str character) :in-position}
+    (contains? (set chosen-word) character) {(str character) :in-word}
+    :else {(str character) :incorrect}))
 
 (defn index-characters
   [word]
@@ -28,3 +38,7 @@
   (check-solution "hello" "stare")
   (check-solution "hello" "heyyo")
   (check-solution "hello" "hello"))
+
+(comment
+  (vec words)
+  (daily-word))
