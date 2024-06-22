202405d41a1dcf8c90b372bee07db2b45e8f7281
Jaxon Adams <jadamsresume1@gmail.com>
2024-06-22 15:58:20 -0600

    added api route for checking a guess against the daily word

--- src/clordle/handler.clj
+++ src/clordle/handler.clj
@@ -1,10 +1,13 @@
 (ns clordle.handler
-  (:require [compojure.core :as server]
+  (:require [clordle.game :as game]
+            [cheshire.core :as json]
+            [compojure.core :as server]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (server/defroutes app-routes
   (server/GET "/" [] "Hello World")
+  (server/GET "/api/check/:guess" [guess] (-> guess game/check-solution json/generate-string))
   (route/not-found "Not Found"))
 
 (def app
