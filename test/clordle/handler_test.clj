3d28a2266cdc8e459c4e156ce607f091bfabd79a
Jaxon Adams <jadamsresume1@gmail.com>
2024-07-06 14:48:33 -0600

    updated tests for request handler

--- test/clordle/handler_test.clj
+++ test/clordle/handler_test.clj
@@ -1,13 +1,14 @@
 (ns clordle.handler-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is testing]]
             [ring.mock.request :as mock]
-            [clordle.handler :refer :all]))
+            [clordle.handler :refer [app]]
+            [clordle.views :refer [index-page]]))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) (index-page)))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
